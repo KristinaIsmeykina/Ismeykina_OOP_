45d50d8fcd1415d9eeaf1aa09fdd362ae306da33
Kristina <kristya.svetka@yandex.ru>
2022-10-17 12:57:34 +0700

    Создание класса FigureBase

---
+++ View/Parallelepiped.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Parallelepiped
+    {
+        /// <summary>
+        /// Высота
+        /// </summary>
+        private double _height;
+
+        /// <summary>
+        /// Длина первой стороны параллелепипида
+        /// </summary>
+        private double _firstSide;
+
+        /// <summary>
+        /// Длина второй стороны параллелепипида
+        /// </summary>
+        private double _secondSide;
+
+        /// <summary>
+        /// Угол между первой и второй сторонной параллелепипеда
+        /// </summary>
+        private double _angle;
+        private const double MaxAngle = 180;
+        private const double MinAngle = 0;
+        private double Height
+        {
+            get => _height;
+            set => _height = CheckValue(value);
+        }
+        private double Angle
+        {
+            get => _angle;
+            set
+            {
+                if (value <= MinAngle || value >= MaxAngle)
+                {
+                    throw new ArgumentOutOfRangeException(
+                        $"{value} must be greater than {MinAngle} " +
+                                $"or less than {MaxAngle}");
+                }
+                _angle = value;
+            }
+        }
+
+        public Parallelepiped(double height,
+                              double firstSide,
+                              double secondSide,
+                              double angle)
+        {
+            
+        }
+
+        public override double Volume => Area * _height;
+    }
+}
